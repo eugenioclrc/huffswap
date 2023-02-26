4a48c11b181b4777684f7b3d4f69c6ba7adb20c5
eugenioclrc <eugenioclrc@gmail.com>
2023-02-25 22:53:10 -0300

    Add liquidity is working :D

--- test/FactoryAddLiquidity.t.sol
+++ test/FactoryAddLiquidity.t.sol
@@ -16,16 +16,6 @@ contract ERC20Mock is ERC20("Mock", "MOCK", 18) {
 }
 
 
-interface IExchange2 {
-    function addLiquidity(uint256 min_liquidity, uint256 max_tokens, uint256 deadline) external payable 
-    // 0 mstore // tokens_to_transfer
-    // 0x20 mstore // liquidity_to_mint
-    // 0x40 mstore // address
-    // 0x60 mstore // max_tokens
-    // 0x80 mstore // min_liquidity
-    returns (uint256 tokens_to_transfer, uint256 liquiditytomint, address, uint256 max_tokensr2, uint256 min_liquidityr);
-}
-
 contract FactoryAddTest is Test {
     /// @dev Address of the SimpleStore contract.
     //SimpleStore public simpleStore;
@@ -40,13 +30,7 @@ contract FactoryAddTest is Test {
     ERC20Mock internal token2 = new ERC20Mock();
 
     function setUp() public {
-        //address _factory = 0x4cf7fafd89861de660f7ebbe75033ab9ed31867b;
-
-        //solExchange = address(new SolExchange(_factory));
-        
-        // address _factory = HuffDeployer.creation_code('Exchange').get_config_with_create_2(2);
         address _factory = 0x9aCe4Afab142FbCBc90e317977a6800076bD64bA;
-        // address _exchange = HuffDeployer.get_config_with_create_2(1);
 
         exchange =
          HuffDeployer.config().with_addr_constant("FACTORY_ADDRESS", _factory).deploy("Exchange");
@@ -67,13 +51,12 @@ contract FactoryAddTest is Test {
         address _token = address(token);
         
         address _exchange = _factory.createExchange(_token);
-        (uint256 tokens_to_transfer, uint256 liquiditytomint, address t, uint256 max_tokensr2, uint256 min_liquidityr)
-        =IExchange2(_exchange).addLiquidity{value: 1000}(1, 1000, block.timestamp + 100); 
 
-        console.log("tokens_to_transfer", tokens_to_transfer);
-        console.log("liquiditytomint", liquiditytomint);
-        console.log("token", t);
-        console.log("max_tokensr2", max_tokensr2);
-        console.log("min_liquidityr", min_liquidityr);
+        vm.expectRevert();
+        IExchange(_exchange).addLiquidity{value: 1000}(1, 1010, block.timestamp + 99); 
+
+        token.approve(_exchange, 1010);
+        IExchange(_exchange).addLiquidity{value: 1000}(1, 1010, block.timestamp + 99); 
+
     }
 }
