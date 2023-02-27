5cff8378a8a25298a37cee59c549c5c8bce8dc47
eugenioclrc <eugenioclrc@gmail.com>
2023-02-27 12:37:37 -0300

    forge fmt

--- test/ExchangeHelper.t.sol
+++ test/ExchangeHelper.t.sol
@@ -8,26 +8,24 @@ import "forge-std/console.sol";
 import {ERC20} from "solmate/tokens/ERC20.sol";
 
 interface Foo {
-    function balance(address token) external view returns (uint256 amount);
-    function addLiquidity(uint256 lpMinted, uint256 maxAmountTokens, address token)
-        external
-        payable
-        returns (uint256 amount, uint256 amount2);
-    function fund() external payable;
+    function addLiquidity(uint256 lpMinted, uint256 maxAmountTokens, address token) external payable returns (uint256 amount, uint256 amount2);
 }
-
 contract ERC20Mock is ERC20("Mock", "MOCK", 18) {
     constructor() {
         _mint(msg.sender, 1000000000000000000000000);
     }
 }
 
+
 contract ExchangeHelperTest is Test {
+    
     address deployed;
     ERC20Mock token;
 
     function setUp() public {
-        deployed = HuffDeployer.config().deploy("mocks/TestExchangeHelp");
+        deployed = HuffDeployer.config().deploy(
+            "mocks/TestExchangeHelp"
+        );
 
         token = new ERC20Mock();
     }
@@ -45,63 +43,36 @@ contract ExchangeHelperTest is Test {
         assertEq(mintLpAmount, expectedMint, "Should have 100 eth"); 
     }
     */
-
-    function calcLiquidity(
-        uint256 total_liquidity,
-        uint256 eth_reserve,
-        uint256 token_reserve,
-        uint256 maxTokens,
-        uint256 value
-    ) internal returns (uint256 liquidity_minted, uint256 token_amount) {
+    
+    
+    function calcLiquidity(uint256 total_liquidity, uint256 eth_reserve, uint256 token_reserve, uint256 maxTokens, uint256 value) internal  pure
+     returns(uint256 liquidity_minted, uint256 token_amount)
+    {
         if (total_liquidity > 0) {
             // nB = (B * nA) / A
             token_amount = value * token_reserve / eth_reserve;
-            liquidity_minted = value * total_liquidity / eth_reserve;
+            liquidity_minted = value * total_liquidity / eth_reserve;     
         } else {
             token_amount = maxTokens;
             liquidity_minted = value;
         }
     }
 
-    function testAddLiquidityFuzz(
-        uint256 lpSupply,
-        uint256 eth_reserve,
-        uint256 tokens_reserve,
-        uint256 maxTokens,
-        uint256 value
-    ) public {
+    function testAddLiquidityFuzz(uint256 lpSupply, uint256 eth_reserve, uint256 tokens_reserve, uint256 maxTokens, uint256 value) public {
         lpSupply = bound(lpSupply, 1 ether, 10 ether);
         eth_reserve = bound(eth_reserve, 1 ether, 10 ether);
         tokens_reserve = bound(tokens_reserve, 1 ether, 10 ether);
         value = bound(value, 1 ether, 10 ether);
         maxTokens = bound(maxTokens, 1 ether, 10 ether);
-
+  
         deal(deployed, eth_reserve);
         deal(address(token), deployed, tokens_reserve);
 
-        (uint256 expectedMint, uint256 expectedTokens) =
-            calcLiquidity(lpSupply, eth_reserve, tokens_reserve, maxTokens, value);
+        (uint256 expectedMint, uint256 expectedTokens) = calcLiquidity(lpSupply, eth_reserve, tokens_reserve, maxTokens, value);   
 
-        (uint256 mintLpAmount, uint256 tokens) =
-            Foo(deployed).addLiquidity{value: value}(lpSupply, maxTokens, address(token));
-
+        (uint256 mintLpAmount, uint256 tokens) = Foo(deployed).addLiquidity{value: value}(lpSupply, maxTokens, address(token));
+       
         assertEq(tokens, expectedTokens, "Should have tokens");
-        assertEq(mintLpAmount, expectedMint, "Should have eth");
-    }
-
-    function testBalance() external {
-        // beware, EOA dont revert so will return 0
-        //(uint256 ret2) = Foo(deployed).balance(address(0));
-        //assertEq(ret2, 0, "Address 0 response empty");
-
-        vm.expectRevert();
-        (uint256 ret2) = Foo(deployed).balance(address(this));
-
-        (ret2) = Foo(deployed).balance(address(token));
-        assertEq(ret2, 0, "No token in contract");
-
-        token.transfer(deployed, 31337);
-        (ret2) = Foo(deployed).balance(address(token));
-        assertEq(ret2, 31337, "Should have 31337 tokens");
+        assertEq(mintLpAmount, expectedMint, "Should have eth");   
     }
 }
--- test/SwapHelp.t.sol
+++ test/SwapHelp.t.sol
@@ -16,19 +16,9 @@ contract PriceCallMacros {
     }
 
     /* events */
-    event EthPurchase(
-        address indexed buyer,
-        address recipient,
-        uint256 tokens_sold,
-        uint256 eth_bought
-    );
+    event EthPurchase(address indexed buyer, address recipient, uint256 tokens_sold, uint256 eth_bought);
 
-    event TokenPurchase(
-        address indexed buyer,
-        address recipient,
-        uint256 eth_sold,
-        uint256 tokens_bought
-    );
+    event TokenPurchase(address indexed buyer, address recipient, uint256 eth_sold, uint256 tokens_bought);
 
     /* errors */
     error ErrDeadlineExpired(uint256 deadline);
@@ -36,12 +26,11 @@ contract PriceCallMacros {
     error ErrEthOutput(uint256 min_eth);
     error ErrTokensOutpur(uint256 min_tokens);
 
-    function ethToTokenSwapInput(
-        uint256 min_tokens,
-        uint256 deadline,
-        address recipient,
-        address tokenAddress
-    ) public payable returns (uint256 tokens_bought) {
+    function ethToTokenSwapInput(uint256 min_tokens, uint256 deadline, address recipient, address tokenAddress)
+        public
+        payable
+        returns (uint256 tokens_bought)
+    {
         if (deadline < block.timestamp) {
             revert ErrDeadlineExpired(deadline);
         }
@@ -53,11 +42,7 @@ contract PriceCallMacros {
         }
 
         uint256 token_reserve = ERC20(tokenAddress).balanceOf(address(this));
-        tokens_bought = getInputPrice(
-            msg.value,
-            address(this).balance - msg.value,
-            token_reserve
-        );
+        tokens_bought = getInputPrice(msg.value, address(this).balance - msg.value, token_reserve);
         // uint256 input_amount,
         // uint256 input_reserve,
         // uint256 output_reserve
@@ -90,39 +75,30 @@ contract PriceCallMacros {
 
         uint256 token_reserve = ERC20(tokenAddress).balanceOf(address(this));
 
-        eth_bought = getInputPrice(
-            tokens_sold,
-            token_reserve,
-            address(this).balance
-        );
+        eth_bought = getInputPrice(tokens_sold, token_reserve, address(this).balance);
         if (eth_bought < min_eth) {
             revert ErrEthOutput(min_eth);
         }
 
         // todo: use safeTransferFrom
-        ERC20(tokenAddress).transferFrom(
-            msg.sender,
-            address(this),
-            tokens_sold
-        );
+        ERC20(tokenAddress).transferFrom(msg.sender, address(this), tokens_sold);
 
         // todo: use safeTransferETH
-        (bool success, ) = recipient.call{value: eth_bought}("");
+        (bool success,) = recipient.call{value: eth_bought}("");
         require(success, "transfer failed");
 
         emit EthPurchase(msg.sender, recipient, tokens_sold, eth_bought);
     }
 
-    function getInputPrice(
-        uint256 input_amount,
-        uint256 input_reserve,
-        uint256 output_reserve
-    ) public pure returns (uint256) {
+    function getInputPrice(uint256 input_amount, uint256 input_reserve, uint256 output_reserve)
+        public
+        pure
+        returns (uint256)
+    {
         unchecked {
             uint256 input_amount_with_fee = input_amount * 997;
             uint256 numerator = input_amount_with_fee * output_reserve;
-            uint256 denominator = (input_reserve * 1000) +
-                input_amount_with_fee;
+            uint256 denominator = (input_reserve * 1000) + input_amount_with_fee;
             return numerator / denominator;
         }
     }
@@ -168,9 +144,7 @@ contract PriceCallMacrosTest is Test {
 
     function setUp() public {
         token = new Token("token", "tkn");
-        helper = PriceCallMacros(
-            HuffDeployer.config().deploy("mocks/TestSwapHelp")
-        );
+        helper = PriceCallMacros(HuffDeployer.config().deploy("mocks/TestSwapHelp"));
         exchange = new PriceCallMacros(address(token));
 
         vm.label(address(token), "token");
@@ -204,21 +178,15 @@ contract PriceCallMacrosTest is Test {
         uint256 value = 10 ether;
 
         vm.prank(user);
-        uint256 expectedTokenBought = exchange.ethToTokenSwapInput{
-            value: value
-        }(min_tokens, deadline, user, address(token));
+        uint256 expectedTokenBought =
+            exchange.ethToTokenSwapInput{value: value}(min_tokens, deadline, user, address(token));
         uint256 expectedTokenBalance = token.balanceOf(user);
         uint256 expectedEthBalance = address(user).balance;
 
         _resetBalances();
 
         vm.prank(user);
-        uint256 actualTokenBought = helper.ethToTokenSwapInput{value: value}(
-            min_tokens,
-            deadline,
-            user,
-            address(token)
-        );
+        uint256 actualTokenBought = helper.ethToTokenSwapInput{value: value}(min_tokens, deadline, user, address(token));
         uint256 actualTokenBalance = token.balanceOf(user);
         uint256 actualEthBalance = address(user).balance;
 
@@ -233,26 +201,14 @@ contract PriceCallMacrosTest is Test {
         uint256 deadline = block.timestamp + 1 days;
 
         vm.prank(user);
-        uint256 expectedEthBought = helper.tokenToEthSwapInput(
-            tokens_sold,
-            min_eth,
-            deadline,
-            user,
-            address(token)
-        );
+        uint256 expectedEthBought = helper.tokenToEthSwapInput(tokens_sold, min_eth, deadline, user, address(token));
         uint256 expectedTokenBalance = token.balanceOf(user);
         uint256 expectedEthBalance = address(user).balance;
 
         _resetBalances();
 
         vm.prank(user);
-        uint256 actualEthBought = exchange.tokenToEthSwapInput(
-            tokens_sold,
-            min_eth,
-            deadline,
-            user,
-            address(token)
-        );
+        uint256 actualEthBought = exchange.tokenToEthSwapInput(tokens_sold, min_eth, deadline, user, address(token));
         uint256 actualTokenBalance = token.balanceOf(user);
         uint256 actualEthBalance = address(user).balance;
 
@@ -264,23 +220,13 @@ contract PriceCallMacrosTest is Test {
     function test_getEthToTokenInputPrice() external {
         uint256 ethSold = 1 ether;
 
-        uint256 expected = exchange.getEthToTokenInputPrice(
-            ethSold,
-            address(token)
-        );
-        uint256 actual = helper.getEthToTokenInputPrice(
-            ethSold,
-            address(token)
-        );
+        uint256 expected = exchange.getEthToTokenInputPrice(ethSold, address(token));
+        uint256 actual = helper.getEthToTokenInputPrice(ethSold, address(token));
 
         assertEq(actual, expected);
     }
 
-    function test_fuzz_getEthToTokenInputPrice(
-        uint256 balance,
-        uint256 tokenBalance,
-        uint256 ethSold
-    ) external {
+    function test_fuzz_getEthToTokenInputPrice(uint256 balance, uint256 tokenBalance, uint256 ethSold) external {
         vm.assume(balance != 0);
         vm.assume(tokenBalance != 0);
         vm.assume(ethSold != 0);
@@ -291,14 +237,8 @@ contract PriceCallMacrosTest is Test {
         deal(address(helper), balance);
         deal(address(token), address(helper), tokenBalance);
 
-        uint256 expected = exchange.getTokenToEthInputPrice(
-            ethSold,
-            address(token)
-        );
-        uint256 actual = helper.getTokenToEthInputPrice(
-            ethSold,
-            address(token)
-        );
+        uint256 expected = exchange.getTokenToEthInputPrice(ethSold, address(token));
+        uint256 actual = helper.getTokenToEthInputPrice(ethSold, address(token));
         assertEq(actual, expected);
     }
 
@@ -313,23 +253,13 @@ contract PriceCallMacrosTest is Test {
         deal(address(helper), balance);
         deal(address(token), address(helper), tokenBalance);
 
-        uint256 expected = exchange.getTokenToEthInputPrice(
-            tokensSold,
-            address(token)
-        );
-        uint256 actual = helper.getTokenToEthInputPrice(
-            tokensSold,
-            address(token)
-        );
+        uint256 expected = exchange.getTokenToEthInputPrice(tokensSold, address(token));
+        uint256 actual = helper.getTokenToEthInputPrice(tokensSold, address(token));
 
         assertEq(actual, expected);
     }
 
-    function test_fuzz_getTokenToEthInputPrice(
-        uint256 balance,
-        uint256 tokenBalance,
-        uint256 tokensSold
-    ) external {
+    function test_fuzz_getTokenToEthInputPrice(uint256 balance, uint256 tokenBalance, uint256 tokensSold) external {
         vm.assume(balance != 0);
         vm.assume(tokenBalance != 0);
         vm.assume(tokensSold != 0);
@@ -340,14 +270,8 @@ contract PriceCallMacrosTest is Test {
         deal(address(helper), balance);
         deal(address(token), address(helper), tokenBalance);
 
-        uint256 expected = exchange.getTokenToEthInputPrice(
-            tokensSold,
-            address(token)
-        );
-        uint256 actual = helper.getTokenToEthInputPrice(
-            tokensSold,
-            address(token)
-        );
+        uint256 expected = exchange.getTokenToEthInputPrice(tokensSold, address(token));
+        uint256 actual = helper.getTokenToEthInputPrice(tokensSold, address(token));
         assertEq(actual, expected);
     }
 
@@ -355,37 +279,17 @@ contract PriceCallMacrosTest is Test {
         uint256 input_amount = 100;
         uint256 input_reserve = 999;
         uint256 output_reserve = 800;
-        uint256 expected = exchange.getInputPrice(
-            input_amount,
-            input_reserve,
-            output_reserve
-        );
+        uint256 expected = exchange.getInputPrice(input_amount, input_reserve, output_reserve);
 
-        uint256 actual = helper.getInputPrice(
-            input_amount,
-            input_reserve,
-            output_reserve
-        );
+        uint256 actual = helper.getInputPrice(input_amount, input_reserve, output_reserve);
         assertEq(actual, expected);
     }
 
-    function test_fuzz_getInputPrice(
-        uint256 input_amount,
-        uint256 input_reserve,
-        uint256 output_reserve
-    ) external {
+    function test_fuzz_getInputPrice(uint256 input_amount, uint256 input_reserve, uint256 output_reserve) external {
         vm.assume(input_reserve != 0);
-        uint256 expected = exchange.getInputPrice(
-            input_amount,
-            input_reserve,
-            output_reserve
-        );
+        uint256 expected = exchange.getInputPrice(input_amount, input_reserve, output_reserve);
 
-        uint256 actual = helper.getInputPrice(
-            input_amount,
-            input_reserve,
-            output_reserve
-        );
+        uint256 actual = helper.getInputPrice(input_amount, input_reserve, output_reserve);
         assertEq(actual, expected);
     }
 }
