ca9ed5ed66c412fb05412c831d7ebe1590d1ba5d
eugenioclrc <eugenioclrc@gmail.com>
2023-02-27 11:40:51 -0300

    deploy script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.15;
 import "foundry-huff/HuffDeployer.sol";
 import "forge-std/Script.sol";
 
-interface SimpleStore {
-  function setValue(uint256) external;
-  function getValue() external returns (uint256);
-}
-
 contract Deploy is Script {
-  function run() public returns (SimpleStore simpleStore) {
-    simpleStore = SimpleStore(HuffDeployer.deploy("SimpleStore"));
-  }
+    function run() public returns (address factory, address exchange) {
+        address _factory = 0xa6b091D1DBA5a3F672D3e4e4D1B6Bd9F53FcD1b6;
+        exchange =
+            HuffDeployer.config().set_broadcast(true).with_addr_constant("FACTORY_ADDRESS", _factory).deploy("Exchange");
+
+        factory = HuffDeployer.config().with_addr_constant("EXCHANGE_IMPLEMENTATION", exchange).set_broadcast(true)
+            .deploy("Factory");
+        console.log("factory", factory);
+    }
 }
